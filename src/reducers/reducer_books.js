b9b16e4ac4964e2e0c6dcc9d2d54c744c74edc2e
lpoliveiraneto <leonidespires@gmail.com>
2017-02-10 10:50:37 -0300

    exemplo de redux do curso udemy

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+	return[
+		{ title: 'Javascript: The good Parts', pages: 100},
+		{ title: 'Harry Potter', pages: 200},
+		{ title: 'The Dark Tower', pages: 300},
+		{ title: 'Eloquent Ruby', pages: 400}
+	];
+}
